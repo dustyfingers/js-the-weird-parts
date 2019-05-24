38f88920c124ca965de9e93f2e02f1551f81622f
dustyfingers <dustyfingers01257@gmail.com>
2019-05-24 13:28:07 -0600

    learned about undefined and code execution

--- sec_2/app.js
+++ sec_2/app.js
@@ -1,8 +1,15 @@
-var a = 'Hello World';
+// let a = 'Hello World';
 
 function b() {
   console.log('Called b!');
 }
 
 b();
+let a;
+console.log(a);
+
+if (a === undefined) console.log('a is undefined');
+
+a = 'Hello World!';
+
 console.log(a);
