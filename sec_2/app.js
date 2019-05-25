37f63407591699dd70848e3a86b1159edbb1f62a
dustyfingers <dustyfingers01257@gmail.com>
2019-05-25 01:24:23 -0600

    learned about functions and execution contexts

--- sec_2/app.js
+++ sec_2/app.js
@@ -1,15 +1,31 @@
-// let a = 'Hello World';
+// HOISTING AND UNDEFINED
+// function b() {
+//   console.log('Called b!');
+// }
+//
+// b();
+//
+// // a === undefined
+// let a;
+// console.log(a);
+//
+// if (a === undefined) console.log('a is undefined');
+//
+// // memory block for a is assigned to string value
+// a = 'Hello World!';
+//
+// console.log(a);
 
+
+// FUNCTIONS, CONTEXT, AND THE EXECUTION STACK
 function b() {
-  console.log('Called b!');
+  console.log('IN B')
 }
 
-b();
-let a;
-console.log(a);
+function a() {
+  b();
+  console.log('IN A');
+  b();
+}
 
-if (a === undefined) console.log('a is undefined');
-
-a = 'Hello World!';
-
-console.log(a);
+a();
