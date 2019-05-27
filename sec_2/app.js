b966e26e29d3734518e1323836d1c9ee15db88ab
dustyfingers <dustyfingers01257@gmail.com>
2019-05-27 01:35:46 -0600

    learned a bunc about variables and scope

--- sec_2/app.js
+++ sec_2/app.js
@@ -17,15 +17,51 @@
 // console.log(a);
 
 
-// FUNCTIONS, CONTEXT, AND THE EXECUTION STACK
-function b() {
-  console.log('IN B')
-}
+// // FUNCTIONS, CONTEXT, AND THE EXECUTION STACK
+// function b() {
+//   console.log('IN B');
+// }
 
-function a() {
-  b();
-  console.log('IN A');
-  b();
-}
+// function a() {
+//   b();
+//   console.log('IN A');
+//   b();
+// }
 
-a();
+// a();
+
+
+// THE SCOPE CHAIN
+// function b() {
+//   console.log('====================================');
+//   console.log(myVar); // returns 1!! not 2 or undefined
+//   console.log('====================================');
+// }
+// function a() {
+//   var myVar = 2;
+//   b();
+// }
+
+// var myVar = 1;
+// a();
+// 
+// ==>put function b inside function a, change lexical environment
+// function a() {
+//   function b() {
+//     console.log('====================================');
+//     console.log(myVar); // returns 2 
+//     console.log('====================================');
+//   }
+//   var myVar = 2;
+//   b();
+// }
+
+// var myVar = 1;
+// a();
+
+
+// SCOPE, LET AND ES6
+// Scope => where a variable is available in your code
+// let => new keyword for creating variables in JS (ES6)
+// let is BLOCK SCOPED & is only available in the block 
+// in which it was declared...even in loops!
