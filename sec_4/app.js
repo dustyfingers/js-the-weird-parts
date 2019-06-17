285535a81e4335ca07c5cc22b61ecb6f62530764
dustyfingers <dustyfingers01257@gmail.com>
2019-06-16 19:25:40 -0600

    object literal syntax

--- sec_4/app.js
+++ sec_4/app.js
@@ -1,14 +1,27 @@
-var person = new Object();
+// var person = new Object();
 
-person['firstname'] = 'Tony';
-person['lastname'] = 'Alicea';
+// person['firstname'] = 'Tony';
+// person['lastname'] = 'Alicea';
 
-console.log(person)
-console.log(person.firstname);
-console.log(person.lastname);
+// console.log(person)
+// console.log(person.firstname);
+// console.log(person.lastname);
 
-person.address = new Object();
-person.address.street = '111 Main St.';
-person.address.city = 'Houston';
-person.address.state = 'Texas';
-console.log(person);
+// person.address = new Object();
+// person.address.street = '111 Main St.';
+// person.address.city = 'Houston';
+// person.address.state = 'Texas';
+// console.log(person);
+
+
+// same as new Object()!
+var Tony = { 
+    firstName: 'Tony',
+    lastName: 'Alicea',
+    address: {
+        street: '111 Main St',
+        city: 'Houston',
+        state: 'Texas'
+    }
+};
+console.log(Tony);
